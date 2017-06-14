6db3f9c2e7acdadae9197c9d60cfc89517e3a672
Yashiro <andy@andydemacbook-pro.local>
2017-06-14 21:34:39 +0800

    Initial Commit

---
+++ qqDemo01/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  qqDemo01
+//
+//  Created by Andy on 17/6/14.
+//  Copyright © 2017年 Andy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ qqDemo01/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  qqDemo01
+//
+//  Created by Andy on 17/6/14.
+//  Copyright © 2017年 Andy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
